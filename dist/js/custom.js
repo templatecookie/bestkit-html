7e38eece9957ccc366a5ceda8efa37cffb0c0566
Zakir Hossen <web.zakirbd@gmail.com>
2021-12-16 21:22:22 +0600

    build: for live site

---
+++ dist/js/custom.js
@@ -0,0 +1,31 @@
+"use strict";
+$(document).ready(function() {
+    $("select").niceSelect();
+
+    AOS.init({
+        once: true,
+    });
+});
+window.addEventListener("load", AOS.refresh);
+
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+      Counter Up Activation
+  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+$(".counter").counterUp({
+    delay: 10,
+    time: 1000,
+});
+
+
+// /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//       Preloader Activation
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+
+$(window).load(function() {
+    setTimeout(function() {
+        $("#loader").fadeOut(500);
+    }, 1000);
+    setTimeout(function() {
+        $("#loader").remove();
+    }, 2000);
+});
